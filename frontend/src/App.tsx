e6960cc5dff9951c071ac41fb233feae3e78e96c
multi92 <milos.mijajlovic@brame.io>
2026-03-31 17:07:52 +0200

    route-update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminRoute from './components/AdminRoute';
+
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import BookingPage from './pages/BookingPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import GalleryPage from './pages/GalleryPage';
+
 import AdminUsersPage from './pages/AdminUsersPage';
 import AdminLoginPage from './pages/AdminLoginPage';
 import AdminBookingsPage from './pages/AdminBookingsPage';
@@ -17,20 +19,29 @@ export default function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+
+          {/* PUBLIC */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+
+          {/* USER PROTECTED */}
           <Route element={<ProtectedRoute />}>
             <Route path="/home" element={<HomePage />} />
             <Route path="/book" element={<BookingPage />} />
             <Route path="/confirmation" element={<ConfirmationPage />} />
             <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="/admin/login" element={<AdminLoginPage />} />
+
+            {/* ADMIN PROTECTED */}
             <Route element={<AdminRoute />}>
               <Route path="/admin/users" element={<AdminUsersPage />} />
               <Route path="/admin/bookings" element={<AdminBookingsPage />} />
             </Route>
           </Route>
+
+          {/* DEFAULT */}
           <Route path="*" element={<Navigate to="/login" replace />} />
+
         </Routes>
       </AuthProvider>
     </BrowserRouter>
